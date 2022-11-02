a142a3b18a6af8728699ce671ffe9498353a7028
Timothy Clarke <timothy.clarke@serverbox.co.uk>
2022-11-02 10:37:57 +0000

    Write the s3 policy as a policy statement that terraform can validate rather than an external template file

--- src/s3.tf
+++ src/s3.tf
@@ -2,7 +2,7 @@
 resource "aws_s3_bucket" "www_bucket" {
   bucket = "www.${var.bucket_name}"
   acl    = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
+  policy = data.aws_iam_policy_document.allow_public_s3_read.json
 
   cors_rule {
     allowed_headers = ["Authorization", "Content-Length"]
@@ -23,11 +23,33 @@ resource "aws_s3_bucket" "www_bucket" {
 resource "aws_s3_bucket" "root_bucket" {
   bucket = var.bucket_name
   acl    = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
+  policy = data.aws_iam_policy_document.allow_public_s3_read.json
 
   website {
     redirect_all_requests_to = "https://www.${var.domain_name}"
   }
 
   tags = var.common_tags
+}
+
+# S3 Allow Public read access as data object
+data "aws_iam_policy_document" "allow_public_s3_read" {
+  statement {
+    sid    = "PublicReadGetObject"
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = "*"
+    }
+
+    resources = [
+      "arn:aws:s3:::${var.bucket_name}/*"
+      "arn:aws:s3:::www-${var.bucket_name}/*"
+    ]
+  }
 }
